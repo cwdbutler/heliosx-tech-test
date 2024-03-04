6374be8e49206148c2dc637422aa5aa3b205613c
Conor Butler <git@conor.tech>
2024-03-04 20:44:35 +0000

    feat: add 'ref' support to TextInput

--- src/components/text-input/text-input.tsx
+++ src/components/text-input/text-input.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from "react";
 import styles from "./text-input.module.css";
 
 interface TextInputProps {
@@ -6,24 +7,33 @@ interface TextInputProps {
   label?: string;
 }
 
-const TextInput = ({
-  children,
-  name,
-  id,
-  label,
-  ...rest
-}: TextInputProps & Omit<React.ComponentPropsWithRef<"input">, "className">) =>
-  // allows for standard input props to be passed but prevents style from being overriden
+export type TextInputField = TextInputProps &
+  Omit<React.ComponentPropsWithRef<"input">, "className">;
+// allows for standard input props to be passed but prevents style from being overriden
 
-  label ? (
-    <div className={styles.component}>
-      <label className={styles.label} htmlFor={id}>
-        {label}
-      </label>
-      <input {...rest} className={styles.input} name={name} id={id} />
-    </div>
-  ) : (
-    <input {...rest} className={styles.input} name={name} id={id} />
-  );
+// TODO: add validation logic
+
+const TextInput = forwardRef(
+  (
+    { name, id, label, ...rest }: TextInputField,
+    ref?: React.ComponentPropsWithRef<"input">["ref"]
+  ) =>
+    label ? (
+      <div className={styles.component}>
+        <label className={styles.label} htmlFor={id}>
+          {label}
+        </label>
+        <input
+          {...rest}
+          className={styles.input}
+          name={name}
+          id={id}
+          ref={ref}
+        />
+      </div>
+    ) : (
+      <input {...rest} className={styles.input} name={name} id={id} ref={ref} />
+    )
+);
 
 export { TextInput };
