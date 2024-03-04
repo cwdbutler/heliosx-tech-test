e0e77d0d1f290e0d67f8967ac0f21048a515559a
Conor Butler <git@conor.tech>
2024-03-04 23:57:16 +0000

    feat: configure breakpoints with custom media queries

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import postcssCustomMedia from "postcss-custom-media";
+import postcssGlobalData from "@csstools/postcss-global-data";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [
+        postcssGlobalData({
+          files: ["src/styles/breakpoints.css"],
+        }),
+        postcssCustomMedia(/* pluginOptions */),
+      ],
+    },
+  },
+});
